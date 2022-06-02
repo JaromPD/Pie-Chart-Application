7945fc2c0d278fc8542901cc6f47eaa5fc1b0661
Jarom Diaz <92468206+JaromPD@users.noreply.github.com>
2022-06-01 23:53:08 -0600

    Added new page, and some interaction.

--- app/src/main/java/com/example/piechartapplication/MainActivity.kt
+++ app/src/main/java/com/example/piechartapplication/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.piechartapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val chartNameInput = findViewById<EditText>(R.id.getChartName)
+        val chartNameString = chartNameInput.text
+        val debugView = findViewById<TextView>(R.id.debugView)
+
+        val createChartButton = findViewById<Button>(R.id.createChartButton)
+        createChartButton.setOnClickListener {
+
+            //REMOVE THIS
+            debugView.text = chartNameString
+
+            val Intent = Intent(this, PieChartActivity::class.java)
+            Intent.putExtra("chartNameString", chartNameString)
+            startActivity(Intent)
+
+        }
     }
 }
--- app/src/main/java/com/example/piechartapplication/PieChartActivity.kt
+++ app/src/main/java/com/example/piechartapplication/PieChartActivity.kt
@@ -2,10 +2,17 @@ package com.example.piechartapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class PieChartActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pie_chart)
+
+        val chartNameString: String? = intent.getStringExtra("chartNameString")
+        val chartNameOutput = findViewById<TextView>(R.id.chartName)
+
+        chartNameOutput.text = chartNameString
     }
 }
